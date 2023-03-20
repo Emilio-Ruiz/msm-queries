8b052a527faf6560197a493b58e74ec9f08308d6
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-20 18:11:15 +0000

    gitt

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,11 @@
 class MoviesController < ApplicationController 
 
   def index
+    
     @list_of_movies = Movie.all
     render({:template => "movie_templates/movies.html.erb"})
+
+  
   end
 
 end 
